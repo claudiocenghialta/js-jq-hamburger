1165ae1356721d26b3eb96ed9744e3a27cceb1c1
Claudio Cenghialta <claudio.cenghialta@gmail.com>
2020-07-22 17:59:56 +0200

    ok migliorati selettori jquery

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-$('.fa-bars').click(function () {
+$('header .header-right a i.fa-bars').click(function () {
     $('.hamburger-menu').fadeIn('slow');
 });
 
-$('.fa-times').click(function () {
+$('header .hamburger-menu a.close i.fa-times').click(function () {
     $('.hamburger-menu').fadeOut('slow');
 });
